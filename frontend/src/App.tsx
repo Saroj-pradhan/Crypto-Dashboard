36c677359000897806ae4d08e74e96a9c7b8ab73
saroj <sarojpradhan7655@gmail.com>
2025-11-08 01:57:21 +0530

    Project setup completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-blue-300 text-5xl m-3.5 inline-block'>All setup is ready</h1>
+    </>
+  )
+}
+
+export default App
